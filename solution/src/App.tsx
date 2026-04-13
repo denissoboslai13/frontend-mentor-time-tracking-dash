bccc1ea976a0bf35cd6cf193d3a5e39abc0b7156
denissoboslai13 <soboslaid@gmail.com>
2026-04-13 21:14:15 +0200

    finished? sortof

--- solution/src/App.tsx
+++ solution/src/App.tsx
@@ -1,16 +1,77 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios'
+import imgJeremy from '../../images/image-jeremy.png'
+import iconEllipsis from '../../images/icon-ellipsis.svg'
+import iconWork from '../../images/icon-work.svg'
+import iconPlay from '../../images/icon-play.svg'
+import iconStudy from '../../images/icon-study.svg'
+import iconExercise from '../../images/icon-exercise.svg'
+import iconSocial from '../../images/icon-social.svg'
+import iconSelfCare from '../../images/icon-self-care.svg'
+
+const Categories = ({ title, icon, timeframes, color, time }) => {
+  return (
+    <div className='h-full flex flex-col w-full'>
+      <div className='rounded-xl flex flex-col items-end overflow-hidden relative w-full h-15' style={{backgroundColor: color}}>
+        <img src={icon} alt="icon-work" className='absolute -top-3 right-3' />
+      </div>
+      <div className='bg-[#1c1f4a] hover:bg-[#34397b] duration-300 flex flex-col p-6 rounded-xl -mt-7 lg:-mt-5 relative flex-grow'>
+        <div className='flex flex-col justify-between items-start'>
+          <p className='text-white lg:mb-7'>{title}</p>
+          <p className='text-3xl lg:text-6xl text-white font-light'>{time === 'day' ? timeframes.daily.current : time === 'week' ? timeframes.weekly.current : timeframes.monthly.current}hrs</p>
+        </div>
+        <div className='absolute right-7 top-8'>
+          <img src={iconEllipsis} alt="icon-ellipsis" />
+        </div>
+        <div className='absolute bottom-7 right-7 lg:left-7'>
+          <p className='text-sm text-[#bdc1ff]'>{time === 'day' ? 'Yesterday' : `Last ${time}`} - {time === 'day' ? timeframes.daily.previous : time === 'week' ? timeframes.weekly.previous : timeframes.monthly.previous}hrs</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const icons = [iconWork, iconPlay, iconStudy, iconExercise, iconSocial, iconSelfCare]
+const colors = ['#ff8c66', '#56c2e6', '#ff5c7c', '#4acf81', '#7536d3', '#f1c65b']
 
 function App() {
-  const [count, setCount] = useState(null)
+  const [categories, setCategories] = useState(0)
+  const [time, setTime] = useState('day')
 
-  axios.get('http://localhost:3001/categories').then(response => {
+  useEffect(() => {
+    axios.get('http://localhost:3001/categories').then(response => {
     console.log(response.data)
-  })
+    setCategories(response.data)
+  })}, []);
 
   return (
-    <div></div>
+    <div className='w-full flex justify-center align-center lg:mt-20'>
+      <div className='flex flex-col lg:grid lg:grid-cols-[1fr_1fr_1fr_1fr] lg:grid-rows-2 justify-center items-center p-5 gap-6 w-full min-w-[375px] max-w-[1050px]'>
+        <div className='lg:row-span-2 lg:col-span-1 h-full w-full'>
+          <div className='bg-[#5847eb] p-6 flex flex-row lg:flex-col lg:items-start lg:gap-8 items-center gap-4 rounded-xl relative z-999 lg:pb-20'>
+            <div className='w-[58px] lg:w-[80px] rounded-full border-2 border-white'>
+              <img src={imgJeremy} alt="image-jeremy" />
+            </div>
+            <div>
+              <p className='text-lg lg:text-lg text-[#bdc1ff] font-extralight'>Report for</p>
+              <p className='text-2xl lg:text-5xl text-white font-light'>Jeremy Robson</p>
+            </div>
+          </div>
+          <div className='bg-[#1c1f4a] flex flex-row lg:flex-col gap-5 justify-evenly lg:justify-start rounded-xl py-10 pb-5 lg:pb-6 lg:pt-10 lg:px-6 -mt-5 z-10 relative text-[#6f76c8]'>
+            <p className={`${time === 'day' ? 'text-[#bdc1ff]' : ''} hover:cursor-pointer hover:text-white duration-300`} onClick={() => setTime('day')}>Daily</p>
+            <p className={`${time === 'week' ? 'text-[#bdc1ff]' : ''} hover:cursor-pointer hover:text-white duration-300`} onClick={() => setTime('week')}>Weekly</p>
+            <p className={`${time === 'month' ? 'text-[#bdc1ff]' : ''} hover:cursor-pointer hover:text-white duration-300`} onClick={() => setTime('month')}>Monthly</p>
+          </div>
+        </div>
+        
+        {categories && (
+          categories.map((c, index) => (
+            <Categories key={c.id} title={c.title} icon={icons[index]} timeframes={c.timeframes} color={colors[index]} time={time}/>
+          ))
+        )}
+      </div>
+    </div>
   )
 }
 
